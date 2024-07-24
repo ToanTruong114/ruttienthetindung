a2abe586497984fe35fa7cd6bc75a906d254f1aa
Truong Ngoc Toan <truongngoctoan114@gmail.com>
2024-07-24 23:40:35 +0700

    ruttienthetindung

---
+++ assets/javascript/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  let date = new Date();
+  let year = date.getFullYear();
+
+  $(".footer").html(`Copyright &copy; Credit Card
+   ${year} all rights reserved`);
+
+});
